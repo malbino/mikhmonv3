f97a561387335a8be3561a7158971f17d8c60d7e
william villca sandoval <villcasandoval@gmail.com>
2020-02-12 11:56:10 -0400

    agregue nueva conexion con infocal en mikrotik para la coneccion

--- include/config.php
+++ include/config.php
@@ -1,3 +1,5 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['infocal'] = array ('1'=>'infocal!192.168.88.1','infocal@|@admin','infocal#|#mZWfoZ8=','infocal%infocal','infocal^infocal','infocal&Rp','infocal*10','infocal(1','infocal)','infocal=10','infocal@!@enable');
