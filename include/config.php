344e85104530a018bae74c8c63d8b8b9638ae155
Mauricio Cartagena <mc973220@gmail.com>
2020-02-12 11:55:08 -0400

    Add new connect with infocal1

--- include/config.php
+++ include/config.php
@@ -1,3 +1,5 @@
 <?php 
 if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
 $data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['Infocal1'] = array ('1'=>'Infocal1!192.168.88.1','Infocal1@|@admin','Infocal1#|#mZWfoZ8=','Infocal1%Infocal1','Infocal1^Infocal1','Infocal1&Rp','Infocal1*10','Infocal1(1','Infocal1)','Infocal1=10','Infocal1@!@disable');
